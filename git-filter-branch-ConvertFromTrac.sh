b25b2feaff84e72aae5dca9404abbde39bb1365d
Hakisho Nukama <nukama@gmail.com>
2015-02-26 22:50:02 +0000

    Renaming scripts

---
+++ git-filter-branch-ConvertFromTrac.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+git filter-branch -f -d /dev/shm/git --tree-filter "~/repositories/github.com/Nukama/scripts/ConvertFromTrac.sh
+" -- --all
--- git-filter-branch-trac2rst.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-git filter-branch -f -d /dev/shm/git --tree-filter "~/repositories/github.com/Nukama/scripts/trac2rst.sh
-" -- --all
