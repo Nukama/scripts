7c7ff9175b704e44d8a6bdf88722b39132046f09
Hakisho Nukama <nukama@gmail.com>
2015-02-27 09:48:36 +0000

    Mailmap

--- git-filter-branch-git-mailmap.sh
+++ git-filter-branch-git-mailmap.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 git filter-branch -f -d /dev/shm/git --commit-filter '
-    R=`echo "$GIT_AUTHOR_NAME <$GIT_AUTHOR_EMAIL>" | git -c mailmap.blob=master:.mailmap check-mailmap --stdin`
+    R=`echo "$GIT_AUTHOR_NAME <$GIT_AUTHOR_EMAIL>" | git -c mailmap.file=/home/user/.mailmap check-mailmap --stdin`
     GIT_AUTHOR_NAME="${R% <*@*>}"
-    R="${R##* <}"
-    GIT_AUTHOR_EMAIL="${R%>}"
-    R=`echo "$GIT_COMMITTER_NAME <$GIT_COMMITTER_EMAIL>" | git -c mailmap.blob=master:.mailmap check-mailmap --stdin`
     GIT_COMMITTER_NAME="${R% <*@*>}"
     R="${R##* <}"
+    GIT_AUTHOR_EMAIL="${R%>}"
     GIT_COMMITTER_EMAIL="${R%>}"
     git commit-tree "$@"
 ' -- --all
