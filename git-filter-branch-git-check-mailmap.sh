ca1de7c2f6f47c70157233c637d9b8e036c0cf46
Hakisho Nukama <nukama@gmail.com>
2015-02-27 10:33:45 +0000

    changed git-mailmap -> git-check-mailmap and updated ConvertFromTrac

--- git-filter-branch-git-check-mailmap.sh
+++ git-filter-branch-git-check-mailmap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git filter-branch -f -d /dev/shm/git --commit-filter '
-    R=`echo "$GIT_AUTHOR_NAME <$GIT_AUTHOR_EMAIL>" | git -c mailmap.file=/home/user/.mailmap check-mailmap --stdin`
+    R=`echo "$GIT_AUTHOR_NAME <$GIT_AUTHOR_EMAIL>" | git -c mailmap.file=/home/user/repositories/github.com/Nukama/scripts/.mailmap check-mailmap --stdin`
     GIT_AUTHOR_NAME="${R% <*@*>}"
     GIT_COMMITTER_NAME="${R% <*@*>}"
     R="${R##* <}"
