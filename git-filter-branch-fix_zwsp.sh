1336181136b6ddd3043e61f1ecc9b623bb44993c
Hakisho Nukama <nukama@gmail.com>
2015-04-23 21:12:11 +0000

    fixed zerowhitespace

--- git-filter-branch-fix_zwsp.sh
+++ git-filter-branch-fix_zwsp.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git filter-branch -f -d /dev/shm/git --tree-filter "python ~/repositories/github.com/woju/qubesos.github.io/_utils/fix_zwsp.py" -- --all
+git filter-branch -f -d /dev/shm/git --tree-filter "for i in `find . -name "*.md" -print0 | xargs -0` ; do python ~/repositories/github.com/woju/qubesos.github.io/_utils/fix_zwsp.py "$i"; done" -- --all
